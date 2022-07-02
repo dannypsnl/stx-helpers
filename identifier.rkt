154d8ee088e17f343759cfa04bcd3ef9e0f8f5a0
Lîm Tsú-thuàn <dannypsnl@gmail.com>
2022-07-03 02:38:44 +0800

    implement `identifier-append`
    This function can concat identifiers to get a new one. Also implement
    the dependency function `string->identifier`, which convert a string to
    an identifier.
    
    Then add some tests to ensure existing functions worked.

--- identifier.rkt
+++ identifier.rkt
@@ -1,5 +1,27 @@
 #lang racket/base
-(provide identifier->string)
+(provide identifier->string
+         string->identifier
+         identifier-append)
 
 (define (identifier->string stx)
   (symbol->string (syntax->datum stx)))
+
+(define (string->identifier str)
+  (datum->syntax #'#f (string->symbol str)))
+
+(define (identifier-append . id*)
+  (string->identifier
+   (apply string-append (map identifier->string id*))))
+
+(module+ test
+  (require rackunit)
+
+  (test-case "convert identifier to string"
+             (check-equal? (identifier->string #'abc) "abc")
+             (check-equal? (identifier->string #'abc2) "abc2")
+             (check-equal? (identifier->string #'+-*/~!@$#%^&*) "+-*/~!@$#%^&*")
+             (check-equal? (identifier->string #'| |) " "))
+  (test-case "convert string to identifier"
+             (check-true (free-identifier=? (string->identifier "abc") #'abc)))
+  (test-case "concat identifiers"
+             (check-true (free-identifier=? (identifier-append #'a #'bc) #'abc))))
